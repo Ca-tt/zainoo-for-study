e357a7480c5eb27cf6ef7c79823c28c7edfe22d8
DL <damir96.lukyanenko@gmail.com>
2021-08-11 18:03:05 +0300

    modal functionality

---
+++ js/page-scroll.js
@@ -0,0 +1,16 @@
+let scrollButton = $(".scroll-down");
+// fix for not working with body
+let body = $("html, body");
+
+scrollButton.click(function (e) {
+	// stop from default click action
+	e.preventDefault();
+	body.animate(
+		{
+			// special non-style property
+			scrollTop: 0,
+		},
+		500,
+		"linear"
+	);
+});
